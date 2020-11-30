0369a9a3c59b6822c9e462f71668cd1a4758bc87
Fernando E. Hartl <fernh_08@hotmail.com>
2020-11-30 19:47:50 -0300

    api bicicletas

--- red-bicicletas/public/javascripts/map.js
+++ red-bicicletas/public/javascripts/map.js
@@ -4,7 +4,14 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 }).addTo(mymap);
 
-var marker = L.marker([-34.64,-58.46]).addTo(mymap);
-var marker = L.marker([-34.62,-58.47]).addTo(mymap);
-var marker = L.marker([-34.63,-58.53]).addTo(mymap);
 
+$.ajax({
+    dataType: 'json',
+    url: 'api/bicicletas',
+    success: function(result){
+        console.log(result);
+        result.bicicletas.forEach(function(bici){
+            L.marker(bici.ubicacion, {title: bici.id}).addTo(map);
+        })
+    }
+})
